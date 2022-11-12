8ffd18b13606f983a7b162ce3aa17faa38c53107
franck <carnet.franck.paul@gmail.com>
2022-11-12 17:37:29 +0100

    Add generated locales

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Fake Me Up'] = '';
+l10n::$locales['The updates have been performed.'] = 'Les mises à jour ont été faites.';
+l10n::$locales['Update Dotclear'] = 'Mettre à jour Dotclear';
+l10n::$locales['No changed filed have been found, nothing to do!'] = 'Pas de fichier modifié trouvé. Rien à faire !';
+l10n::$locales['The following files will have their checksum faked:'] = 'Les fichiers suivants vont voir leur checksum modifié :';
+l10n::$locales['The following files digests will have their checksum cleaned:'] = 'Les fichiers suivants vont avoir leur checksum nettoyé :';
+l10n::$locales['Still ok to continue'] = 'Toujours d\'accord pour continuer';
+l10n::$locales['Fake Me Up has already been run once.'] = 'Fake Me Up a déjà été lancé une fois.';
+l10n::$locales['Remove the backup digest file, I want to play again'] = 'Supprimer le fichier de sauvegarde du digest, je veux relancer le processus';
+l10n::$locales['Continue'] = 'Continuer';
+l10n::$locales['Please read carefully the following disclaimer before proceeding!'] = 'Merci de lire avec attention la mise en garde suivante avant de continuer !';
+l10n::$locales['I have read and understood the disclaimer and wish to continue anyway.'] = 'J\'ai lu et j\'ai compris la mise en garde, je souhaite continuer quand même.';
