60019ff7979176ba347b6e6794e4228024e086ed
franck <carnet.franck.paul@gmail.com>
2024-09-04 11:11:11 +0200

    New release 4.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Fake Me Up',
     'Fakes Dotclear digest to force automatic updates',
     'Bruno Hondelatte',
-    '4.1',
+    '4.2',
     [
         'requires' => [['core', '2.28']],
         'type'     => 'plugin',
