e599d260c924e9e6c2d2d554e7474e466fafc404
franck <contact@open-time.net>
2026-08-13 16:40:54 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,19 +11,23 @@
  * @copyright Bruno Hondelatte
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'Fake Me Up',
-    'Fakes Dotclear digest to force automatic updates',
-    'Bruno Hondelatte',
-    '7.0',
-    [
-        'date'     => '2026-08-03T09:56:33+0200',
-        'requires' => [['core', '2.39']],
-        'type'     => 'plugin',
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=fakemup',
-        'support'    => 'https://github.com/franck-paul/fakemup',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/fakemeup/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Fake Me Up',
+        'Fakes Dotclear digest to force automatic updates',
+        'Bruno Hondelatte',
+        '7.0',
+        [
+            'date'     => '2026-08-03T09:56:33+0200',
+            'requires' => [['core', '2.39']],
+            'type'     => 'plugin',
+
+            'details'    => 'https://open-time.net/?q=fakemup',
+            'support'    => 'https://github.com/franck-paul/fakemup',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/fakemeup/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
